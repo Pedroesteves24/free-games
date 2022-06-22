b627f36b11dba8a40ffba34ba13766e9f21cc45a
Pedroesteves24 <pedro2002esteves@gmail.com>
2022-06-22 14:28:15 +0100

    first commit

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,10 +6,10 @@ import { BsInstagram } from 'react-icons/bs';
 export function Footer() {
     return(
         <footer>
-            <div className="email"><a href="https://www.freetogame.com/" target="_blank">Free To Game Site</a></div>
+            <div className="email"><a href="https://www.freetogame.com/" target="_blank" rel="noreferrer">Free To Game Site</a></div>
             <ul className="social">                                      
-                <li><a href="https://www.facebook.com/ " target="_blank"> <BsFacebook/> </a></li>
-                <li><a href="https://www.instagram.com/" target="_blank"> <BsInstagram/> </a></li>
+                <li><a href="https://www.facebook.com/ " target="_blank" rel="noreferrer"> <BsFacebook/> </a></li>
+                <li><a href="https://www.instagram.com/" target="_blank" rel="noreferrer"> <BsInstagram/> </a></li>
             </ul>
         </footer>
     )
--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -4,8 +4,8 @@ import './styles.scss'
 export function Game({id, thumbnail, name}){
     return(
         <article className="card">
-            <Link to = {`game/${id}`}>
-            <div className="card-image"><img src={thumbnail} /></div>
+            <Link to ={`game/${id}`}>
+            <div className="card-image"><img src={thumbnail} alt={id} /></div>
             <div className="card-title">{name}</div>
             </Link>
         </article>
